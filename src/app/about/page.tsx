1038836b873896fcb76dc09e459a66bad3094376
Bhargavi Vedula <vedulabhargavi19@gmail.com>
2026-07-23 14:35:59 +0530

    Add About page component with basic content

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <div style={{
+      background: 'white',
+      padding: '30px',
+      borderRadius: '10px'
+    }}>
+      <h1>About</h1>
+      <p>This is a simple game project built with Next.js.</p>
+    </div>
+  )
+}
